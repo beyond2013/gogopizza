3bea7b7eae2b793d4a10c666044154ab9a172b27
beyond2013 <imran.cs.uob@gmail.com>
2020-09-14 15:32:21 +0500

    Revert to commit: 2dc05b6b

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,6 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("jquery")
-require("bootstrap")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -17,6 +15,3 @@ require("bootstrap")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-//
-import "bootstrap"
-import "../stylesheets/application.scss"
